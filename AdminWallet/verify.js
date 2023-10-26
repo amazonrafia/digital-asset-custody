ea633b3b1f9fb8f552680c15f62e6e90c3fd8045
EC2 Default User <ec2-user@ip-10-0-0-92.ec2.internal>
2023-10-26 00:00:24 +0000

    added Eth verification

---
+++ AdminWallet/verify.js
@@ -0,0 +1,35 @@
+import { ethers } from "ethers";
+import { SecretsManagerClient,GetSecretValueCommand} from "@aws-sdk/client-secrets-manager";
+
+let GoerliTbaUrl;
+let adminAddress=process.env.ADMIN_ETH_ADDRESS;
+let chainProvider;
+
+let getAccountEthBalance=async ()=>{
+    console.log(`Admin account ${adminAddress} has ${await chainProvider.getBalance(adminAddress)} Ethers`);
+};
+
+
+(async () => {
+    let secret_name = "GoerliAccess";
+    let secClient = new SecretsManagerClient({region: "us-east-1"});
+    try {
+        let response = await secClient.send(
+            new GetSecretValueCommand({
+                SecretId: secret_name,
+                VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+            })
+        );
+        GoerliTbaUrl = JSON.parse(response.SecretString)["ambtbaurl"];
+        chainProvider = ethers.getDefaultProvider(GoerliTbaUrl);
+        await getAccountEthBalance();
+
+    }
+    catch (error) {
+        throw error;
+    }
+})();
+
+
+
+ 
